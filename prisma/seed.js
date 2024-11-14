29acca2334f6e1636e1cd9c10013a8a07fa0df55
EngineerMoore <alexiashalise@gmail.com>
2024-11-14 17:00:51 -0600

    employee database seeded with 10 employees

---
+++ prisma/seed.js
@@ -0,0 +1,19 @@
+const prisma = require(`../prisma`);
+const { faker } = require(`@faker-js/faker`)
+
+const seed = async() => {
+  const employees = [];
+  for (let i=0; i < 10; i++){
+    employees.push({name: faker.person.fullName()});
+  };
+  await prisma.employee.createMany({
+    data: employees
+  })
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
